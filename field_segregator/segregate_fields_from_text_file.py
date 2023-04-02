650f168f1c96037d7efe76986821bbf431744016
Vivek Gupta <vivg269@gmail.com>
2023-04-03 01:49:17 +1000

    Better documentation, and removed the redundant field_len list

--- field_segregator/segregate_fields_from_text_file.py
+++ field_segregator/segregate_fields_from_text_file.py
@@ -4,30 +4,87 @@ import pandas as pd
 import tqdm
 
 
-def segregate_fields(inp_file, out_file, field_names, field_lens, byte_starts, byte_ends):
+def segregate_fields(inp_file, out_file, field_names, byte_starts, byte_ends):
+    '''
+    Segregates the fields based on the field byte_start and byte_end values
+    Reads in the data row by row from 'inp_file' and writes the segregated
+    output to 'out_file'.
+
+    Params
+    ------
+    inp_file: str
+            Path to the data file
+    out_file: str
+            Path to the output data file after segregation
+    field_names: list
+            A list of strings, which contain the field/variable names
+    byte_starts: list
+            A list of integers, which indicate the starting byte (1-indexed, inclusive range)
+            of the values in each row
+    byte_ends: list
+            A list of integers, which indicate the end byte (1-indexed, inclusive range)
+            of the values in each row
+    '''
+
+    #Open the input file and read all the lines as string
     with open(inp_file) as f:
         lines = f.readlines()
 
+    #If outname is not provided, generate a default one by changing the extension to .csv
     if out_file is None:
         out_file = inp_file.strip(".txt") + ".csv"
+
+    #Open the out_file in 'write' mode
     o = open(out_file, 'w')
+
     ncols = len(field_names)
+    
+    #Write out the field_names/keys/variables first as column headers
     for icol in range(ncols):
         o.write(field_names[icol] + ",")
     o.write("\n")
+
+    print("Now segregating the data fields line by line....")
+    #Now loop over the data file line by line and split the values using the parsed info
     for ii, line in enumerate(tqdm.tqdm(lines)):
         #print(ii)
         oline = ""
         for icol in range(ncols):
             key = field_names[icol]
             #print(f"key = {key}, byte_start = {byte_starts[icol]}, byte_ends = {byte_ends[icol]}")
-            value = line[int(byte_starts[icol]) - 1 : int(byte_ends[icol])]
+            value = line[int(byte_starts[icol]) - 1 : int(byte_ends[icol])]     #byte_starts and byte_ends are 1-indexed inclusive-range endpoint markers
             oline += value
             oline += ","
-        o.write(oline.strip(",") + "\n")
+        o.write(oline.strip(",") + "\n")        #Get rid of the last comma, and put a new-line instead
 
 
 def parse_excel_file(fname, header):
+    '''
+    Parses the excel file and looks for the desired section. It returns a list of
+    variable names (keys) and the byte_starts, byte_ends of the values.
+
+    Params
+    ------
+    fname: str
+            Path to the excel file which contains the variable info
+    header: str
+            The name of the section which has to be parsed
+            It is case-sensitive
+
+    Returns
+    -------
+    field_names: list
+            A list of strings, which contain the field/variable names
+    byte_starts: list
+            A list of integers, which indicate the starting byte (1-indexed, inclusive range)
+            of the values in each row
+    byte_ends: list
+            A list of integers, which indicate the end byte (1-indexed, inclusive range)
+            of the values in each row
+
+
+    '''
+
     print(f"Parsing variable information from excel file - {fname}")
     f = pd.read_excel(fname)
     nrows = len(f)
@@ -37,7 +94,6 @@ def parse_excel_file(fname, header):
     field_names = []
     byte_starts = []
     byte_ends = []
-    field_lens = []
     while True:
         irow +=1
         row = f.iloc[irow]
@@ -51,12 +107,10 @@ def parse_excel_file(fname, header):
             #This line is within the target section... read the values and go to next line
             #print("Extracting info from row - ", row)
             field_name = row[1]
-            field_len = row[4]
             byte_start = row[5]
             byte_end = row[6]
 
             field_names.append(field_name)
-            field_lens.append(field_len)
             byte_starts.append(byte_start)
             byte_ends.append(byte_end)
 
@@ -69,7 +123,7 @@ def parse_excel_file(fname, header):
             print(f"Could not find the section {header} in the file {fname}!")
             sys.exit(1)
 
-    return field_names[1:-1], field_lens[1:-1], byte_starts[1:-1], byte_ends[1:-1]
+    return field_names[1:-1], byte_starts[1:-1], byte_ends[1:-1]
 
 
 def get_parser():
@@ -85,8 +139,8 @@ def get_parser():
 
 def main():
     args = get_parser()
-    a, b, c, d = parse_excel_file(args.excel_file, args.section.strip())
-    segregate_fields(args.datafile, args.outfile, a[1:], b[1:], c[1:], d[1:])
+    a, b, c  = parse_excel_file(args.excel_file, args.section.strip())
+    segregate_fields(args.datafile, args.outfile, a[1:], b[1:], c[1:])
 
 if __name__ == '__main__':
     main()
