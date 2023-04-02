2899a903ef81fef57e2707b19c2da80a46d8bccc
Vivek Gupta <vivg269@gmail.com>
2023-04-03 01:19:40 +1000

    First setup.py

--- setup.py
+++ setup.py
@@ -3,3 +3,14 @@ from setuptools import setup
 
 requirements = ['numpy']
 
+setup(name="field_segregator", 
+      version=0.1,
+      description="A module to seperate the fields from a text file",
+      author="Vivek Gupta",
+      author_email="vivg269@gmail.com",
+      install_requires=requirements,
+      python_requires='>=3.6',
+      packages=['field_segregator'],
+      entry_points={'console_scripts':['sep_fields=field_segregator.segregate_fields_from_text_file:main']}
+      )
+
